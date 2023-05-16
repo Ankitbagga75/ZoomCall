a5fc4181f5bd86cabc8e92b812ff5cf9d02b9c26
Ankitbagga75 <ankitbagga7572@gmail.com>
2023-05-17 00:47:47 +0530

    17-5

---
+++ js/room_rtc.js
@@ -0,0 +1,43 @@
+const APP_ID = "88259f29840643ad8f4f4bbc29abd87a"
+
+let uid = sessionStorage.getItem('uid')
+if (!uid){
+    uid = String(Math.floor(Math.random()*10000))
+    sessionStorage.setItem('uid',uid)
+}
+
+let token = null;
+let client;
+
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+let roomId = urlParams.get('room')
+
+if (!roomId){
+    roomId = window.location = "room.html?room=1223"
+}
+// roomID = room.html?room=1223
+
+
+let localTracks = []
+let remoteUsers = {
+}
+
+let joinRoomInit = async()=>{
+    client =  AgoraRTC.createClient({mode:'rtc',codec:'vp8'})
+    await client.join(APP_ID,roomId,token,uid)
+    joinStream()
+}
+
+let joinStream = async()=>{
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+    
+    let player = `<div class="video_container id="user-container-${uid}">
+                     <div class ="video-player" id="user-${uid}"></div>
+                  </div>`
+
+    document.getElementById('streams_container').insertAdjacentHTML('beforeend')
+    localTracks[1].play(`user-${uid}`)
+}
+
+joinRoomInit()
