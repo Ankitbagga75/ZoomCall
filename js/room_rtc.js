254ca9a320a24a2b7150aab6cec5c5b44e12a538
Ankitbagga75 <ankitbagga7572@gmail.com>
2023-05-18 00:36:01 +0530

    Adding Multiple user

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -14,7 +14,7 @@ const urlParams = new URLSearchParams(queryString)
 let roomId = urlParams.get('room')
 
 if (!roomId){
-    roomId = window.location = "room.html?room=1223"
+    roomId ='main'
 }
 // roomID = room.html?room=1223
 
@@ -26,6 +26,7 @@ let remoteUsers = {
 let joinRoomInit = async()=>{
     client =  AgoraRTC.createClient({mode:'rtc',codec:'vp8'})
     await client.join(APP_ID,roomId,token,uid)
+    client.on('user-published',handleUserPublished)
     joinStream()
 }
 
@@ -36,8 +37,34 @@ let joinStream = async()=>{
                      <div class ="video-player" id="user-${uid}"></div>
                   </div>`
 
-    document.getElementById('streams_container').insertAdjacentHTML('beforeend')
+    document.getElementById('streams_container').insertAdjacentHTML('beforeend',player)
     localTracks[1].play(`user-${uid}`)
+    await client.publish([localTracks[0],localTracks[1]])
 }
 
+let handleUserPublished = async(user, mediaType)=>{
+    debugger
+    remoteUsers[user.uid] = user
+    await client.subscribe(user,mediaType)
+    let player = document.getElementById(`user-container-${user.uid}`)
+    if (player === null){
+     player = `<div class="video_container id="user-container-${user.uid}">
+                     <div class ="video-player" id="user-${user.uid}"></div>
+                  </div>`
+
+    document.getElementById('streams_container').insertAdjacentHTML('beforeend',player)
+    }
+    if (mediaType === 'video'){
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+
+    if (mediaType === 'audio'){
+        user.audioTrack.play(`user-${user.uid}`)
+    }
+}
+
+let handleUserLeftFunction = async(user)=>{
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
 joinRoomInit()
